648623534a5762eccd9a7722798a388c44b58aa8
Hőbör Viktor <vkhobor@gmail.com>
2020-04-19 04:16:20 +0200

    Edit line on branch-A

--- src/main/java/hu/bme/mit/spaceship/GT4500.java
+++ src/main/java/hu/bme/mit/spaceship/GT4500.java
@@ -77,7 +77,7 @@ public class GT4500 implements SpaceShip {
         break;
 
       case ALL:
-        // try to fire both of the torpedo stores
+        // try to fire both of the torpedo stores *edit this part for test*
         if(!primaryTorpedoStore.isEmpty() && !secondaryTorpedoStore.isEmpty())
           {
             firingSuccess= secondaryTorpedoStore.fire(1) || primaryTorpedoStore.fire(1);
